a5c32f85a00e4859a98bd5f406dc0e916ca46100
Paul Miller <paul@pauljmiller.com>
2019-05-13 22:30:48 -0700

    fmt

--- piet-raqote/examples/basic-raqote.rs
+++ piet-raqote/examples/basic-raqote.rs
@@ -10,24 +10,24 @@ const TEXTURE_HEIGHT: i32 = 100;
 // const HIDPI: f64 = 2.0;
 
 fn main() {
-  let test_picture_number = std::env::args()
-    .skip(1)
-    .next()
-    .and_then(|s| s.parse::<usize>().ok())
-    .unwrap_or(0);
+    let test_picture_number = std::env::args()
+        .skip(1)
+        .next()
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or(0);
 
-  let mut draw_target = DrawTarget::new(TEXTURE_WIDTH, TEXTURE_HEIGHT);
+    let mut draw_target = DrawTarget::new(TEXTURE_WIDTH, TEXTURE_HEIGHT);
 
-  // TODO: Replace this with clear
-  draw_target.clear(SolidSource {
-    r: 0xFF,
-    g: 0xFF,
-    b: 0xFF,
-    a: 0xFF,
-  });
+    // TODO: Replace this with clear
+    draw_target.clear(SolidSource {
+        r: 0xFF,
+        g: 0xFF,
+        b: 0xFF,
+        a: 0xFF,
+    });
 
-  let mut raqote_context = RaqoteRenderContext::new(&mut draw_target);
-  draw_test_picture(&mut raqote_context, test_picture_number).unwrap();
+    let mut raqote_context = RaqoteRenderContext::new(&mut draw_target);
+    draw_test_picture(&mut raqote_context, test_picture_number).unwrap();
 
-  draw_target.write_png("temp-raqote.png").unwrap();
+    draw_target.write_png("temp-raqote.png").unwrap();
 }
--- piet-raqote/examples/test-raqote.rs
+++ piet-raqote/examples/test-raqote.rs
@@ -1,33 +1,37 @@
 use raqote::{DrawOptions, DrawTarget, PathBuilder, SolidSource, Source};
 
 fn main() {
+    let mut draw_target = DrawTarget::new(400, 400);
 
-  let mut draw_target = DrawTarget::new(400, 400);
+    let blue = Source::Solid(SolidSource {
+        r: 0x0,
+        g: 0x0,
+        b: 0xFF,
+        a: 0xFF,
+    });
 
-  let blue = Source::Solid(SolidSource { r: 0x0, g: 0x0, b: 0xFF, a: 0xFF });
+    let mut pb = PathBuilder::new();
+    pb.move_to(10.0, 10.0);
+    pb.line_to(10.0, 110.0);
+    pb.line_to(110.0, 110.0);
+    pb.line_to(110.0, 60.0);
 
-  let mut pb = PathBuilder::new();
-  pb.move_to(10.0, 10.0);
-  pb.line_to(10.0, 110.0);
-  pb.line_to(110.0, 110.0);
-  pb.line_to(110.0, 60.0);
-  
-  //Finish without closing
-  let path = pb.finish();
-  
-  draw_target.fill(&path, &blue, &DrawOptions::new());
+    //Finish without closing
+    let path = pb.finish();
 
-  let mut pb = PathBuilder::new();
-  pb.move_to(120.0, 10.0);
-  pb.line_to(120.0, 110.0);
-  pb.line_to(220.0, 110.0);
-  pb.line_to(220.0, 60.0);
+    draw_target.fill(&path, &blue, &DrawOptions::new());
 
-  //Close, then finish
-  pb.close();
-  let path = pb.finish();
+    let mut pb = PathBuilder::new();
+    pb.move_to(120.0, 10.0);
+    pb.line_to(120.0, 110.0);
+    pb.line_to(220.0, 110.0);
+    pb.line_to(220.0, 60.0);
 
-  draw_target.fill(&path, &blue, &DrawOptions::new());
+    //Close, then finish
+    pb.close();
+    let path = pb.finish();
 
-  draw_target.write_png("test-raqote.png").unwrap();
+    draw_target.fill(&path, &blue, &DrawOptions::new());
+
+    draw_target.write_png("test-raqote.png").unwrap();
 }
